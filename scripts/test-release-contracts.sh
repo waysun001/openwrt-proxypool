cdcd3c2decc4c8a87b4d8b486c6ca06c5977a84d
waysun001 <waysun001@proton.me>
2026-08-08 18:45:23 +0800

    fix: advance LuCI package version

--- scripts/test-release-contracts.sh
+++ scripts/test-release-contracts.sh
@@ -62,6 +62,8 @@ workflow_jobs() {
 require_fixed "$MAKEFILE" 'PKG_BUILD_DEPENDS:=golang/host'
 require_fixed "$MAKEFILE" 'PKG_VERSION:=2.0.0'
 require_fixed "$MAKEFILE" 'PKG_RELEASE:=13'
+require_fixed "$LUCI_MAKEFILE" 'PKG_VERSION:=2.0.2'
+require_fixed "$LUCI_MAKEFILE" 'PKG_RELEASE:=1'
 require_fixed "$MAKEFILE" 'GO_PKG:=proxypoold'
 require_fixed "$MAKEFILE" 'GO_PKG_BUILD_PKG:=$(GO_PKG)/cmd/proxypoold $(GO_PKG)/cmd/proxypoolctl'
 require_fixed "$MAKEFILE" 'GO_PKG_LDFLAGS_X:=$(GO_PKG)/internal/buildinfo.Version=$(PKG_VERSION)'
